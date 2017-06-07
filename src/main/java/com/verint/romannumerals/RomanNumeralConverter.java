c1535a9a235c6616a850ae7a51900fb56f2ced3b
R. David Dunphy <rdd.dunphy@gmail.com>
2017-06-07 11:23:01 +0100

    Initial commit

---
+++ src/main/java/com/verint/romannumerals/RomanNumeralConverter.java
@@ -0,0 +1,52 @@
+package com.verint.romannumerals;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class RomanNumeralConverter {
+
+    private static Map<Integer, String> arabicToRoman;
+    private static Set<Integer> subtractables;
+
+    static {
+        arabicToRoman = new LinkedHashMap<>();
+        arabicToRoman.put(1000, "M");
+        arabicToRoman.put(500, "D");
+        arabicToRoman.put(100, "C");
+        arabicToRoman.put(50, "L");
+        arabicToRoman.put(10, "X");
+        arabicToRoman.put(5, "V");
+        arabicToRoman.put(1, "I");
+
+        subtractables = new HashSet<>();
+        subtractables.addAll(Arrays.asList(1, 10, 100));
+    }
+
+    public static String convertToRoman(int arabic) {
+        for (Integer subtractable : subtractables) {
+            if (arabicToRoman.containsKey(arabic + subtractable)) {
+                return arabicToRoman.get(subtractable) + arabicToRoman.get(arabic + subtractable);
+            }
+        }
+
+        int largestSingleNumeralArabicValue = getLargestSingleNumeralArabicValue(arabic);
+        String roman = arabicToRoman.get(largestSingleNumeralArabicValue);
+        int remainder = arabic - largestSingleNumeralArabicValue;
+        if (remainder != 0) {
+            roman += convertToRoman(remainder);
+        }
+        return roman;
+    }
+
+    private static int getLargestSingleNumeralArabicValue(int arabic) {
+        for (int arabicKey : arabicToRoman.keySet()) {
+            if (arabicKey <= arabic) {
+                return arabicKey;
+            }
+        }
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/verint/romannumeralstest/RomanNumeralTest.java
@@ -0,0 +1,115 @@
+package com.verint.romannumeralstest;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.verint.romannumerals.RomanNumeralConverter;
+
+public class RomanNumeralTest {
+
+    @Test
+    public void shouldConvertArabicOneToRomanI() {
+        assertThat(RomanNumeralConverter.convertToRoman(1), is("I"));
+    }
+
+    @Test
+    public void shouldConvertArabicFiveToRomanV() {
+        assertThat(RomanNumeralConverter.convertToRoman(5), is("V"));
+    }
+
+    @Test
+    public void shouldConvertArabicTenToRomanX() {
+        assertThat(RomanNumeralConverter.convertToRoman(10), is("X"));
+    }
+
+    @Test
+    public void shouldConvertArabicFiftyToRomanL() {
+        assertThat(RomanNumeralConverter.convertToRoman(50), is("L"));
+    }
+
+    @Test
+    public void shouldConvertArabicHundredToRomanC() {
+        assertThat(RomanNumeralConverter.convertToRoman(100), is("C"));
+    }
+
+    @Test
+    public void shouldConvertArabicFiveHundredToRomanD() {
+        assertThat(RomanNumeralConverter.convertToRoman(500), is("D"));
+    }
+
+    @Test
+    public void shouldConvertArabicOneThousandToRomanM() {
+        assertThat(RomanNumeralConverter.convertToRoman(1000), is("M"));
+    }
+
+    @Test
+    public void shouldConvertArabicTwoToRepeatingI() {
+        assertThat(RomanNumeralConverter.convertToRoman(2), is("II"));
+    }
+
+    @Test
+    public void shouldConvertArabicThreeToTripleRepeatingI() {
+        assertThat(RomanNumeralConverter.convertToRoman(3), is("III"));
+    }
+
+    @Test
+    public void shouldConvertArabicSixToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(6), is("VI"));
+    }
+
+    @Test
+    public void shouldConvertArabicFifteenToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(15), is("XV"));
+    }
+
+    @Test
+    public void shouldConvertArabicSixtyToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(60), is("LX"));
+    }
+
+    @Test
+    public void shouldConvertArabicOneHundredAndFiftyToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(150), is("CL"));
+    }
+
+    @Test
+    public void shouldConvertArabicSixHundredToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(600), is("DC"));
+    }
+
+    @Test
+    public void shouldConvertArabicOneThousandFiveHundredToMixedNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(1500), is("MD"));
+    }
+
+    @Test
+    public void shouldConvertGreatFireOfLondonToRomanNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(1666), is("MDCLXVI"));
+    }
+
+    @Test
+    public void shouldConvertArabicFourToSubtractionOfNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(4), is("IV"));
+    }
+
+    @Test
+    public void shouldConvertArabicNineToSubtractionOfNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(9), is("IX"));
+    }
+
+    @Test
+    public void shouldConvertArabicFourteenToAdditionAndSubtractionOfNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(14), is("XIV"));
+    }
+
+    @Test
+    public void shouldConvertArabicNinetyToSubtractionOfNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(90), is("XC"));
+    }
+
+    @Test
+    public void shouldConvertArabicNineHundredToSubtractionOfNumerals() {
+        assertThat(RomanNumeralConverter.convertToRoman(900), is("CM"));
+    }
+}
